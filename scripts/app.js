b60b808e195a42a552cb2b365ad7b971a33d64e0
max-b10 <bungaymax@gmail.com>
2021-06-02 15:12:49 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-console.log("app.js running");
+const weaponButtons = document.querySelectorAll(".choose-weapon");
+const rulesButton = document.querySelector("#rules-button");
+const rulesPage = document.querySelector(".mobile-rules");
+const closeRulesButton = document.querySelector("#close-rules-button");
+
+rulesButton.addEventListener("click", () => {
+  rulesPage.style.display = "block";
+  closeRulesButton.style.display = "block";
+});
+closeRulesButton.addEventListener("click", () => {
+  rulesPage.style.display = "none";
+  closeRulesButton.style.display = "none";
+});
