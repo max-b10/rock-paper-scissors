f0b4094de1006f21a3fb7b363435b0c45ac7a042
max-b10 <bungaymax@gmail.com>
2021-06-08 16:15:51 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -28,7 +28,7 @@ const weaponButtonsArray = Array.from(weaponButtons);
 for (let i = 0; i < weaponButtonsArray.length; i++) {
   weaponButtonsArray[i].addEventListener("click", () => {
     weaponButtonContainer.style.display = "none";
-    resultsDiv.style.display = "block";
+    resultsDiv.style.display = "flex";
   });
 }
 // let playerChoice = "";
@@ -39,37 +39,77 @@ scissorsButton.addEventListener("click", (e) => {
   let playerChoice = "scissors";
   console.log(playerChoice);
   computerWeapon();
+  generateResultsDOM(playerChoice, computerChoice);
   // setTimeout(computerWeapon, 1500);
 });
 spockButton.addEventListener("click", (e) => {
   let playerChoice = "spock";
   console.log(playerChoice);
   computerWeapon();
+  generateResultsDOM(playerChoice, computerChoice);
 });
 paperButton.addEventListener("click", (e) => {
   let playerChoice = "paper";
   console.log(playerChoice);
   computerWeapon();
+  generateResultsDOM(playerChoice, computerChoice);
 });
 lizardButton.addEventListener("click", (e) => {
   let playerChoice = "lizard";
   console.log(playerChoice);
   computerWeapon();
+  generateResultsDOM(playerChoice, computerChoice);
 });
 rockButton.addEventListener("click", (e) => {
   let playerChoice = "rock";
   console.log(playerChoice);
   computerWeapon();
+  generateResultsDOM(playerChoice, computerChoice);
 });
 // Computer random weapon selection:
 computerWeapon = () => {
   const weaponNumber = Math.floor(Math.random() * 5);
   computerChoice = computerWeapons[weaponNumber];
-  console.log(computerChoice);
-  // console.log(`Comp choice: ${computerChoice}`);
+  console.log(`Comp choice: ${computerChoice}`);
 };
 
-// Need a display outcome function:
+// Generate and display outcome DOM:
+const generateResultsDOM = (playerChoice, computerChoice) => {
+  // Player Results DOM:
+  let playerResultDiv = document.createElement("div");
+  playerResultDiv.classList.add("playerResultDiv");
+
+  let playerResultPara = document.createElement("p");
+  playerResultPara.classList.add("playerResultPara");
+  playerResultPara.innerHTML = "YOU PICKED";
+
+  let playerResultImage = document.createElement("img");
+  playerResultImage.classList.add("playerResultImage", "choose-weapon");
+  playerResultImage.id = `${playerChoice}`;
+  playerResultImage.setAttribute("src", `/images/icon-${playerChoice}.svg`);
+
+  resultsDiv.appendChild(playerResultDiv);
+  playerResultDiv.appendChild(playerResultPara);
+  playerResultDiv.appendChild(playerResultImage);
+
+  // Computer Results DOM:
+  let compResultDiv = document.createElement("div");
+  compResultDiv.classList.add("compResultDiv");
+
+  let compResultPara = document.createElement("p");
+  compResultPara.classList.add("compResultPara");
+  compResultPara.innerHTML = "HOUSE PICKED";
+
+  let compResultImage = document.createElement("img");
+  compResultImage.classList.add("compResultImage", "choose-weapon");
+  compResultImage.id = `${computerChoice}`;
+  compResultImage.setAttribute("src", `/images/icon-${computerChoice}.svg`);
+
+  resultsDiv.appendChild(compResultDiv);
+  compResultDiv.appendChild(compResultPara);
+  compResultDiv.appendChild(compResultImage);
+};
+
 // Need a check winner function:
 
 // if (playerChoice === computerChoice) {
